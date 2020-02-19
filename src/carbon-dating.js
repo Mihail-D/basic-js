c79c1bd9acdfe674e9ffbecc4f84b40241ba5540
Mihail-D <nuby.4register@gmail.com>
2020-02-19 22:27:47 +0300

    feat: add 'Carbon dating' subtask

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,20 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+	let sampleActivityToInt = parseFloat(sampleActivity);
+
+	if (!sampleActivity || typeof sampleActivity !== "string" || isNaN(Number.parseFloat(sampleActivity))) {
+		return false;
+	}
+
+	if (sampleActivityToInt <= 0 || sampleActivityToInt >= 15) {
+		return false;
+	}
+
+	let logOfTwo = 0.693;
+	let coefficient = logOfTwo / HALF_LIFE_PERIOD;
+	let carbonDating = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivityToInt) / coefficient);
+
+	return carbonDating;
 };
