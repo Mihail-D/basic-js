c487512cee9402dd3eb370b218b2bcbf021ca320
Mihail-D <nuby.4register@gmail.com>
2020-02-21 16:50:07 +0300

    feat: add 'Hanoi Tower' task

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,6 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-}
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    const turnCount = 2 ** disksNumber - 1;
+	const turnSpeedInSeconds = turnsSpeed / 3600;
+    
+	return { turns: turnCount, seconds: turnCount / turnSpeedInSeconds };
+};
