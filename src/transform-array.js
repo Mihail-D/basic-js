9065151e5ce8f27b6b4cca71b32a8836e39c0ac7
Mihail-D <nuby.4register@gmail.com>
2020-02-22 14:30:48 +0300

    feat: add 'Transfom Array' task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,25 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+	let newArray = [];
+	if (arr.length < 1) {
+		return [];
+	}
+	if (!(arr instanceof Array)) throw new Error();
+
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] == "--discard-next") {
+			++i;
+		} else if (arr[i] == "--discard-prev") {
+			if (newArray.length > 0) {
+				newArray = newArray.slice(0, newArray.length - 1);
+			}
+		} else if (arr[i] == "--double-next") {
+			if (++i < arr.length) {
+				newArray.push(arr[i]);
+				newArray.push(arr[i]);
+			}
+		} else if (arr[i] == "--double-prev") {
+			if (i > 0) newArray.push(arr[i - 1]);
+		} else newArray.push(arr[i]);
+	}
+	return newArray;
 };
