2eb3a92cda259c1f77f05aa0015df7de4252a649
Mihail-D <nuby.4register@gmail.com>
2020-03-21 11:04:27 +0300

    add: Extended repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,42 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str,options)
+{
+	if (typeof str !== 'string')
+	{
+		str = String(str);
+	}
+	if (options.separator === undefined)
+	{
+		options.separator = '+';
+	}
+	if (options.additionRepeatTimes === undefined)
+	{
+		options.additionRepeatTimes = 1;
+	}
+	if (options.additionSeparator === undefined)
+	{
+		options.additionSeparator = '';
+	}
+	if (options.addition === undefined)
+	{
+		options.additionRepeatTimes = 0;
+	}
+	if (options.repeatTimes === undefined)
+	{
+		options.repeatTimes = 1;
+	}
+
+	let resultString = '';
+	for (let i = 0; i < options.repeatTimes; i++)
+	{
+		resultString = resultString + str;
+		for (let k = 0; k < options.additionRepeatTimes; k++)
+		{
+			resultString = resultString + options.addition;
+			if (k + 1 !== options.additionRepeatTimes)
+				resultString = resultString + options.additionSeparator;
+		}
+		if (i + 1 !== options.repeatTimes)
+			resultString = resultString + options.separator;
+	}
+	return resultString;
 };
-  
